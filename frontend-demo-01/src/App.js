ed5263caf71e9b535b5571de5e6e5d84748e6dd0
Taehun Kim <tkim@assurance.com>
2019-09-02 15:28:44 -0700

    before sass

--- frontend-demo-01/src/App.js
+++ frontend-demo-01/src/App.js
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from 'react'
+import axios from 'axios'
+import Confetti from 'react-dom-confetti'
+import './App.scss'
+
+
+const API_ENDPOINT = 'https://pixabay.com/api/'
+const API_KEY = '13417145-d0c367819415b077de5e950e3'
 
 function App() {
+    const input = useRef(null)
+    const [images, setImages] = useState([])
+
+    const search = async () => {
+        const q = input.current.value
+        console.log(`searching for ${q}`)
+
+        const { data } = await axios.get(API_ENDPOINT, {
+            params: {
+                key: API_KEY,
+                per_page: 50,
+                q
+            }
+        })
+
+        setImages(data.hits)
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='root'>
+        <header>
+          <h1>Frontend DEMO</h1>
+        </header>
+
+        <section className='search'>
+            <input ref={input} />
+            <button onClick={search}>SEARCH?</button>
+            <Confetti active={!!images.length} />
+
+        </section>
+        <section className='images'>
+           {images.map(image => (
+               <img key={image.id} src={image.previewURL} height={100} alt={image.id} />
+           ))}
+        </section>
+      </div>
   );
 }
 
